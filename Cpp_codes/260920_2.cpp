1c0f7f84f50fc8bba86ad416f2f5b632c64d62cd
Kang, ChungHyeok <kch30507@gmail.com>
2026-09-20 14:34:45 +0000

    260920 ~p73 study

---
+++ Cpp_codes/260920_2.cpp
@@ -0,0 +1,93 @@
+#include <iostream>
+
+class Date {
+    int month_days[12] = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+    private:
+        int year_;
+        int month_;
+        int day_;
+    
+    public:
+        void SetDate(int year, int month, int date) {
+            year_ = year;
+            month_ = month;
+            day_ = date;
+        }
+        void AddDay(int inc) {
+            if (inc + day_ > month_days[month_ - 1]) {
+                int past_day = day_;
+                day_ = (inc + day_) % month_days[month_ - 1];
+                month_ += (inc + past_day) / month_days[month_ - 1];
+            }
+        }
+        void AddMonth(int inc) {
+            if (inc + month_ > 12) {
+                year_ += (inc + month_) / 12;
+                month_ = (inc + month_) % 12;
+            }
+        }
+        void AddYear(int inc) {
+            year_ += inc;
+        }
+
+        void ShowDate() {
+            std::cout << "Year : " << year_ << std::endl
+            << "Month : " << month_ << std::endl
+            << "Day : " << day_ << std::endl; 
+        }
+};
+
+int main() {
+    std::cout << "날짜 설정 : 1" << std::endl
+    << "일 덧셈 계산 : 2" << std::endl
+    << "월 덧셈 계산 : 3" << std::endl
+    << "년 덧셈 계산 : 4" << std::endl
+    << "날짜 보기 : 5" << std::endl
+    << "나가기 : 0" << std::endl;
+    
+    int input;
+    Date date;
+    while (1) {
+        std::cout << std::endl << "입력하세요 : ";
+        std::cin >> input;
+        if (input == 0) break;
+        if (input == 1) {
+            int input_year;
+            int input_month;
+            int input_day;
+            std::cout << "날짜를 설정하세요." << std::endl
+            << "Year : ";
+            std::cin >> input_year;
+            std::cout << "Month : ";
+            std::cin >> input_month;
+            std::cout << "Day : ";
+            std::cin >> input_day;
+            date.SetDate(input_year, input_month, input_day);
+        }
+        if (input == 2) {
+            int inc;
+            std::cout << "며칠을 더할까요? : ";
+            std::cin >> inc;
+            date.AddDay(inc);
+            std::cout << "설정이 완료되었습니다." << std::endl;
+        }
+        if (input == 3) {
+            int inc;
+            std::cout << "몇 달을 더할까요? : ";
+            std::cin >> inc;
+            date.AddMonth(inc);
+            std::cout << "설정이 완료되었습니다." << std::endl;
+        }
+        if (input == 4) {
+            int inc;
+            std::cout << "몇 년을 더할까요? : ";
+            std::cin >> inc;
+            date.AddYear(inc);
+            std::cout << "설정이 완료되었습니다." << std::endl;
+        }
+        if (input == 5)
+            date.ShowDate();
+    }
+
+    return 0;
+}
